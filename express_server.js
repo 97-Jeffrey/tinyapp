d39497f424f6fca2f37f3981b6316ec6d168bc46
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-04 02:04:03 +0000

    now user could be directed to their intended address from generated shortURL

--- express_server.js
+++ express_server.js
@@ -50,7 +50,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log('longURL===',longURL);
+  console.log(req.params.shortURL);
+  res.redirect(longURL);
+})
 
 
 app.post("/urls", (req, res) => {
