cc40a3a8afc85f52bf12a3a84c10beb739367d32
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-04 07:51:14 +0000

    one more POST routes are added to delete any url in the url list

--- express_server.js
+++ express_server.js
@@ -67,6 +67,12 @@ app.post("/urls", (req, res) => {
   
 });
 
+app.post("/urls/:shortURL/delete", (req, res)=>{
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+})
+
+
 app.listen(PORT, () => {
   console.log(`Example app is listening on port ${PORT}`);
 });
