eddb09634b9d865e1b6e53f686d616499295981b
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-07 03:24:08 +0000

    added another helper function for return the password

--- helpers.js
+++ helpers.js
@@ -1,4 +1,4 @@
-const getUserbyEmail = function (email, database) {
+const getUserByEmail = function (email, database) {
   let result;
   let arr = Object.keys(database);
   for (let user of arr) {
@@ -10,5 +10,16 @@ const getUserbyEmail = function (email, database) {
   return result;
 }
 
+const getUserByEmail2 = function(email,users){
+  let result;
+  let arr = Object.keys(users);
+  for (let user of arr) {
+    if (users[user].email === email) {
+      result = users[user].password;
+      break;
+    }
+  }
+  return result;
+}
 
-module.exports = { getUserbyEmail };
+module.exports = { getUserByEmail, getUserByEmail2 };
